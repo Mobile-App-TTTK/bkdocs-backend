fdce7647965fea32bcf509ea6c4dddb40eb23491
mahara0511 <khanhzip14@gmail.com>
2025-10-02 16:00:39 +0700

    fix: ci-cd

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 // Ưu tiên đọc biến từ file _FILE (khi dùng Docker secrets)
 function getEnv(key: string, defaultValue?: string): string | undefined {
-  const fileKey = process.env[`${key}_FILE`];
+  const fileKey = process.env[`${key}`];
   if (fileKey && fs.existsSync(fileKey)) {
     return fs.readFileSync(fileKey, 'utf8').trim();
   }
@@ -19,9 +19,9 @@ export default new DataSource({
   type: 'postgres',
   host: getEnv('DATABASE_HOST', 'localhost'),
   port: Number(getEnv('DATABASE_PORT', '5432')),
-  username: getEnv('DATABASE_USER', 'postgres'),
-  password: getEnv('DATABASE_PASSWORD', 'postgres'),
-  database: getEnv('DATABASE_NAME', 'appdb'),
+  username: getEnv('DATABASE_USER', 'mobileuser'),
+  password: getEnv('DATABASE_PASSWORD', 'mobilepass'),
+  database: getEnv('DATABASE_NAME', 'mobileappdb'),
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false, // KHÔNG bật trong production
