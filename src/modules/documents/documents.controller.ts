75ae41fce76852fbf4cfae7f1e320a8e46bd0792
mahara0511 <khanhzip14@gmail.com>
2025-10-13 00:13:52 +0700

    Add: Example for download production

--- src/modules/documents/documents.controller.ts
+++ src/modules/documents/documents.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Param, Res, Logger, UseGuards } from '@nestjs/common';
 import { DocumentsService } from './documents.service';
-import { Response } from 'express';
 import { JwtAuthGuard } from '@common/guards/jwt-auth.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags, ApiParam } from '@nestjs/swagger';
 import { DownloadDocumentUrlResponseDto } from './dtos/responses/downloadDocumentUrl.response.dto';
 
 @ApiTags('documents')
@@ -16,6 +15,12 @@ export class DocumentsController {
 
 
   @Get(':id/download')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'ID của tài liệu cần tải xuống',
+    example: '884330b0-3ab1-4ca3-a0f2-3929c9c39933',
+  })
   async download(@Param('id') id: string) {
       const url : string = await this.documentsService.getDownloadUrl(id);
       return new DownloadDocumentUrlResponseDto({ url });
